1299887538834edd18c386ebd700dbef18155629
Rajesh <rajesh@Admins-MacBook-Pro.local>
2023-12-13 12:11:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socket) => {
+  socket.on("amountSend", (data) => {});
+});
+
+server.listen(3002, () => {
+  console.log("user connected");
+});
