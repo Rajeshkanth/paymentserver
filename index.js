b7980b195e09afd5931abb787e3718f516bc4a22
Rajesh <rajesh@Admins-MacBook-Pro.local>
2023-12-14 19:01:51 +0530

    removed uniqueid

--- index.js
+++ index.js
@@ -19,41 +19,28 @@ const io = new Server(server, {
   },
 });
 
-var val = 1;
-const alertSockets = new Map();
+var val = 0;
 
 io.on("connection", (socket) => {
   console.log(`user connected ${(val += 1)}`);
   socket.on("paymentPageConnected", (data) => {
-    const { socketId, uniqueId } = data;
     console.log(data.NewReceiver, data.socketId, uniqueId);
     if (data.connected) {
-      alertSockets.set(socketId, uniqueId, socket),
-        io.emit("paymentConfirmAlert", {
-          receivedValu: data.NewReceiver,
-          UniqueId: uniqueId,
-          SocketId: socketId,
-        });
+      io.emit("paymentConfirmAlert", {
+        receivedValu: data.NewReceiver,
+      });
     }
   });
   socket.on("clicked", (data) => {
     console.log(data);
     const { UniqueId } = data;
     if (data.clicked) {
-      const specificSocket = alertSockets.get(data.socketId);
-      // if (specificSocket) {
-      //   specificSocket.emit("success", true);
-      // }
       io.to(UniqueId).emit("success", true);
     }
   });
   socket.on("canceled", (data) => {
     if (data.cancel) {
-      const specificSocket = alertSockets.get(data.socketId); // Get the specific socket
-      if (specificSocket) {
-        specificSocket.emit("failed", true);
-      }
-      // io.to(data.socketId).emit("failed", true);
+      io.to(data.socketId).emit("failed", true);
     }
   });
 });
